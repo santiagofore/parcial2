82e9be92f40115455d4c57e72c17cbe8cd910140
santiagofore <sforeron@ufpso.edu.co>
2024-11-20 20:21:29 -0500

    (doc)documentos:se realizaron cambios

---
+++ java.js
@@ -0,0 +1,8 @@
+
+// Control del menú hamburguesa
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
